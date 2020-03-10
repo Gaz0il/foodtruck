59bcf777c7040fdd421635e80f417cf932a4a4df
Gaz0il <gaz0il50@gmail.com>
2020-03-10 14:36:53 +0100

    repo finish

--- foodtruck/src/main/java/com/example/demo/repo/FamilleRepasRepo.java
+++ foodtruck/src/main/java/com/example/demo/repo/FamilleRepasRepo.java
@@ -2,6 +2,6 @@ package com.example.demo.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.*;
 
-public interface FamilleRepasRepo JpaRepository<FamilleRepas,Integer>{
+public interface FamilleRepasRepo extends JpaRepository<FamilleRepas,Integer>{
 
 }
