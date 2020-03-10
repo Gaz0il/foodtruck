1488ea0ae03b7b650c3f45f0b6dd57da64683834
Gaz0il <gaz0il50@gmail.com>
2020-03-10 14:22:11 +0100

    repo

--- foodtruck/src/main/java/com/example/demo/repo/GenreRepo.java
+++ foodtruck/src/main/java/com/example/demo/repo/GenreRepo.java
@@ -1,8 +1,7 @@
-
 package com.example.demo.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.*;
 
-public interface ProfilRepo extends JpaRepository<Profil,Integer> {
+public interface GenreRepo extends JpaRepository<Genre,Integer> {
 
 }
